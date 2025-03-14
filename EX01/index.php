ddbc0e4ed5d2688fe3ac6b44accdf9de66cd7f09
LarissaSirioFerraz <larissasirioferraz@gmail.com>
2025-03-14 20:51:01 -0300

    Imprimir na tela do index.php pedindo a inserção de dados

---
+++ EX01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Valor Maior</title>
+</head>
+<body>
+<fieldset>
+    <legend>Valor Maior</legend>
+    <form method="post" action="calcular.php">
+        Digite um número:
+        <input type="text" name="n1" size="25" />
+        <br />
+        Digite outro número:
+        <input type="text" name="n2" size="25" />
+        <br />
+    </form>
+</fieldset>
+</body>
+</html>
