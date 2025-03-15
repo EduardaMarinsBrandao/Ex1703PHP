32e1897b3487bc9b0be09aa68dc23d002629f32c
EduardaMarinsBrandao <duda13brandao@gmail.com>
2025-03-14 21:35:23 -0300

    Cálculo do IMC

--- Ex04/calcular.php
+++ Ex04/calcular.php
@@ -1,27 +1,42 @@
-<!DOCTYPE HTML>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Média das notas</title>
+    <title>Cálculo IMC</title>
 </head>
 <body>
 <?php
     //Declara as variáveis e captura os dados do formulário
-    $n1 = $_POST['n1'];
-	$n2 = $_POST['n2'];
-	$n3 = $_POST['n3'];
-	$n4 = $_POST['n4'];
+    $n1 = $_POST['peso'];
+	$n2 = $_POST['altura'];
 	
-    // Calculando a média
-    $soma = ($n1 + $n2 + $n3 + $n4) / 4;
+    // Calculando o IMC da pessoa
+    $imc = $n1 / ($n2 * $n2);
+    echo "Seu IMC é: $imc <br />";
 
-    if ($soma > 5)
+    if ( $imc < 18.5 )
     {
-        echo "Aprovado";
+        echo "Você está abaixo do peso.";
     }
-    else
-    echo "Reprovado";
-
+    else if ( $imc >= 18.5 && $imc < 24.9 )
+    {
+        echo "Você está com o peso normal.";
+    }
+    else if ( $imc >= 25 && $imc < 29.9 )
+    {
+        echo "Você está com sobrepeso.";
+    }
+    else if ( $imc >= 30 && $imc < 34.9 )
+    {
+        echo "Você está com obesidade grau 1.";
+    }
+    else if ( $imc >= 35 && $imc < 39.9 )
+    {
+        echo "Você está com obesidade grau 2.";
+    }
+    else if ( $imc >= 40 )
+    {
+        echo "Você está com obesidade grau 3.";
+    }
 ?>
 </body>
 </html>
